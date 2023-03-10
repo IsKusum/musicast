3726ac1a6d13ab6557ce480f97977124fb5c081d
Kusum Swarnakar <kusumswarnakar20@gmail.com>
2023-03-10 22:30:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function myFunction() {
-    var element = document.getElementsByClassName("container");
-    element.classList.toggle("dark-mode");
-}
+    var element = document.body;
+    element.classList.toggle("light-mode");
+  }
+
+  console.log("welcome to my music app");
+  let songIndex = 0;
+  let audioElement = new Audio('Doraemon.mp3');
+  let songs= {
+    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
+    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
+    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
+    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
+    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
+  }
+  audioElement.play();
+
+  document.addEventListener('time')
