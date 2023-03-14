a9754b21f0094ee60ab104ddbe3ef68e2f900d7e
Kusum Swarnakar <kusumswarnakar20@gmail.com>
2023-03-14 13:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,29 @@ function myFunction() {
 
   console.log("welcome to my music app");
   let songIndex = 0;
-  let audioElement = new Audio('Doraemon.mp3');
-  let songs= {
-    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
-    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
-    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
-    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
-    {songName: "Doraemon theme song",filePath:"songs/Doraemon.mp3",coverPath: "images\Doraemon.jpg"},
-  }
+  let audioElement = new Audio('songs/Doraemon.mp3');
+  let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+  let songs = [
+    {songName: "Doraemon theme song",filePath: "songs/Doraemon.mp3",coverPath:"images/doraemon.jpg"},
+    {songName: "Ben 10 theme song",filePath: "songs/ben.mp3",coverPath:"images/Ben 10.jpg"},
+    {songName: "Kiteretsu theme song",filePath: "songs/kit.mp3",coverPath:"images/Kiteretsu.jpg"},
+    {songName: "Doraemon theme song",filePath:"songs/pok.mp3",coverPath:"images/pokemon.jpg"},
+    {songName: "Doraemon theme song",filePath:"songs/shin.mp3",coverPath:"images/Shinchan.jpg"},
+  ]
+  masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+      audioElement.play();
+      masterPlay.classList.remove('fa-sharp fa-regular fa-circle-pause');
+    }
+  })
+  audioElement.addEventListener('timeupdate',()=>{
+//console.log('timeupdate');
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+  })
   audioElement.play();
 
-  document.addEventListener('time')
+  
+  
